3ea80b705a6bda1916f93741dfcec92c94c96135
kaiyu li <krisfk@gmail.com>
2021-04-07 01:31:49 +0800

    .

--- wp-content/themes/bo-shu/header.php
+++ wp-content/themes/bo-shu/header.php
@@ -22,7 +22,11 @@
     <link rel="profile" href="https://gmpg.org/xfn/11">
 
     <?php wp_head(); ?>
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous">
+    </script>
 </head>
 
 <body <?php body_class(); ?>>
@@ -32,7 +36,11 @@
 		?>
 
     <header id="site-header" class="header-footer-group" role="banner">
-        fdsf
+
+
+        <div class="top-bar">
+            <div></div>
+        </div>
 
     </header><!-- #site-header -->
 
