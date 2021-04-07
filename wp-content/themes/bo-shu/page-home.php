8b954c20d4bc5a5a53fc0b1b522a8d27dd6cf0dd
kaiyu li <krisfk@gmail.com>
2021-04-08 03:58:20 +0800

    .

--- wp-content/themes/bo-shu/page-home.php
+++ wp-content/themes/bo-shu/page-home.php
@@ -134,7 +134,7 @@ get_header();
 
         <h3 class="home-color-heading pink-orange text-light mt-5">Lastest news</h3>
         <div class="latest-news-div">
-            <ul>
+            <ul class="mt-4">
                 <li><a href="#">Multi-function pouch 使用介紹</a></li>
                 <li><a href="#">木口金袋選用手冊</a></li>
                 <li><a href="#">Marimekko 懶人包</a></li>
