6fa2ec82aef832f7ea6439343b11b5f89591784b
kaiyu li <krisfk@gmail.com>
2021-04-08 05:02:21 +0800

    .

--- wp-content/themes/bo-shu/footer.php
+++ wp-content/themes/bo-shu/footer.php
@@ -24,7 +24,7 @@
 
 
                 <div class="subscribe-form-div">
-                    <input type="text" class="form-text border-0" placeholder="Email address...">
+                    <input type="text" class="form-text border-0 subscribe-input" placeholder="Email address...">
                     <a href="#" class="subscribe-btn btn">Subscribe</a>
                 </div>
                 <br><br>
