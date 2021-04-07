a95349d8201ca2ed05e0634d17b6c85f0e943c97
kaiyu li <krisfk@gmail.com>
2021-04-08 00:21:02 +0800

    .

--- wp-content/themes/bo-shu/page-home.php
+++ wp-content/themes/bo-shu/page-home.php
@@ -35,7 +35,7 @@ get_header();
 
                 <div class="d-table w-100 h-100">
 
-                    <div class="d-table-cell align-center">
+                    <div class="d-table-cell align-middle">
                         <div class="text-center text-light">
                             <h2>訂製產品</h2>
                             厭倦了大眾化或倒模式袋款？ <br>
