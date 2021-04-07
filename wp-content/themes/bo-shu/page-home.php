4c79f7a838d7609c094a3882f783dc03dd28d175
kaiyu li <krisfk@gmail.com>
2021-04-08 00:01:29 +0800

    .

--- wp-content/themes/bo-shu/page-home.php
+++ wp-content/themes/bo-shu/page-home.php
@@ -27,11 +27,11 @@ get_header();
 
         <div class="row g-0">
 
-            <div class="col-10">
+            <div class="col-7">
                 <img class="w-100" src="https://bo-shu.shop/wp-content/uploads/2021/04/banner-temp-img.jpg" alt="">
             </div>
 
-            <div class="col-2 pink-blue">
+            <div class="col-5 pink-blue">
                 fdsf
             </div>
         </div>
