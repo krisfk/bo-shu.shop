c4ff194e4f70f295c280283b0a75996421e83027
kaiyu li <krisfk@gmail.com>
2021-04-08 03:48:23 +0800

    .

--- wp-content/themes/bo-shu/page-home.php
+++ wp-content/themes/bo-shu/page-home.php
@@ -116,12 +116,12 @@ get_header();
         <div class="row mt-5">
 
             <div class="col">
-                <div class="text-center">Marimekko collection</div>
+                <div class="text-center main-banners-txt">Marimekko collection</div>
 
                 <img class="w-100" src="https://bo-shu.shop/wp-content/uploads/2021/04/photo5.jpg" alt="">
             </div>
             <div class="col">
-                <div class="text-center">Home collection</div>
+                <div class="text-center main-banners-txt">Home collection</div>
 
                 <img class="w-100" src="https://bo-shu.shop/wp-content/uploads/2021/04/photo6.jpg" alt="">
             </div>
