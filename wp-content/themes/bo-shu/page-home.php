372a57a8546dbda3cdc814bacc184593105b4e1b
kaiyu li <krisfk@gmail.com>
2021-04-08 04:25:32 +0800

    .

--- wp-content/themes/bo-shu/page-home.php
+++ wp-content/themes/bo-shu/page-home.php
@@ -147,9 +147,9 @@ get_header();
 
         <h3 class="home-color-heading pink-orange text-light mt-5">Latest products</h3>
         <ul class="latest-products-div mt-4">
-            <li><img src="https://bo-shu.shop/wp-content/uploads/2021/04/photo7.jpg" alt=""></li>
-            <li><img src="https://bo-shu.shop/wp-content/uploads/2021/04/photo8.jpg" alt=""></li>
-            <li><img src="https://bo-shu.shop/wp-content/uploads/2021/04/photo9.jpg" alt=""></li>
+            <a><img src="https://bo-shu.shop/wp-content/uploads/2021/04/photo7.jpg" alt=""></a>
+            <a><img src="https://bo-shu.shop/wp-content/uploads/2021/04/photo8.jpg" alt=""></a>
+            <a><img src="https://bo-shu.shop/wp-content/uploads/2021/04/photo9.jpg" alt=""></a>
 
 
         </ul>
