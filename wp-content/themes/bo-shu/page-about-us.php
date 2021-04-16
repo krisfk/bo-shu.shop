276eefee2f0cbf9b976f7aac52bf8098576c43b4
kaiyu li <krisfk@gmail.com>
2021-04-17 00:09:55 +0800

    .

--- wp-content/themes/bo-shu/page-about-us.php
+++ wp-content/themes/bo-shu/page-about-us.php
@@ -54,6 +54,24 @@ get_header();
             香港手工製作 ． 獨特 ． 個性化手袋品牌</div>
 
 
+        <h2 class="mt-5">
+            <img class="potato" src="<?php echo get_template_directory_uri() .'/assets/images/potato.png';?>" alt="">
+            品牌里程碑 <img class="potato" src="<?php echo get_template_directory_uri() .'/assets/images/potato.png';?>"
+                alt="">
+        </h2>
+
+        <div class="text-center mt-3">「布薯」成立於2018年，由2個香港女生建立的香港本地手作品牌， <br>
+            目標令個性化的袋款成為新的主流，改變大眾對倒模式設計袋款的依賴，<br>
+            製作出一個只屬於“你”的袋！<br>
+            亦可於盛行速食時裝的年代提供一個替代選擇。<br>
+            「布薯」所有產品均為限量製作，採用優質布料並將其化身成獨特的手袋。<br><br>
+
+            我們除了提供世界各地的精選布料，更有自家設計的布款圖案，<br>
+            所有布藝產品都是香港手工製作和專業訂做。<br><br>
+
+            香港手工製作 ． 獨特 ． 個性化手袋品牌</div>
+
+
 
 
 
