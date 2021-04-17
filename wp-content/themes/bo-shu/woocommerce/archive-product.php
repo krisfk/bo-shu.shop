63a0ba11a740504dab1becd43abd795c8da90e57
kaiyu li <krisfk@gmail.com>
2021-04-18 05:25:28 +0800

    .

--- wp-content/themes/bo-shu/woocommerce/archive-product.php
+++ wp-content/themes/bo-shu/woocommerce/archive-product.php
@@ -29,12 +29,13 @@ get_header( 'shop' );
 do_action( 'woocommerce_before_main_content' );
 
 ?>
+test
 <header class="woocommerce-products-header">
-	<?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
-		<h1 class="woocommerce-products-header__title page-title"><?php woocommerce_page_title(); ?></h1>
-	<?php endif; ?>
+    <?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
+    <h1 class="woocommerce-products-header__title page-title"><?php woocommerce_page_title(); ?></h1>
+    <?php endif; ?>
 
-	<?php
+    <?php
 	/**
 	 * Hook: woocommerce_archive_description.
 	 *
