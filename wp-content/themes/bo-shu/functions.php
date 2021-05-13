8cf476c5eaf5e9f8392f23906d421952c0b8907e
kaiyu li <krisfk@gmail.com>
2021-05-14 07:21:28 +0800

    .

--- wp-content/themes/bo-shu/functions.php
+++ wp-content/themes/bo-shu/functions.php
@@ -810,6 +810,6 @@ function wp_get_menu_array($current_menu='Main Menu') {
 
 function move_variation_price() {
     remove_action( 'woocommerce_single_variation', 'woocommerce_single_variation', 10 );
-    add_action( 'woocommerce_after_add_to_cart_quantity', 'woocommerce_single_variation', 10 );
+    add_action( 'woocommerce_before_variations_form', 'woocommerce_single_variation', 10 );
 }
 add_action( 'woocommerce_before_add_to_cart_form', 'move_variation_price' );
