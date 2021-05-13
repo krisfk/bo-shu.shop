39c6cb9b303a71712ade2750df90240ad3787d7a
kaiyu li <krisfk@gmail.com>
2021-05-14 06:05:33 +0800

    .

--- wp-content/themes/bo-shu/woocommerce/content-single-product.php
+++ wp-content/themes/bo-shu/woocommerce/content-single-product.php
@@ -33,6 +33,8 @@ if ( post_password_required() ) {
 ?>
 
 <div id="product-<?php the_ID(); ?>" <?php wc_product_class( '', $product ); ?>>
+    <?php echo do_shortcode('[woo-currency-switcher]')?>
+
 
     <?php
 	/**
